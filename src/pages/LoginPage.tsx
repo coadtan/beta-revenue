794992e099e242a49eebb14f8ccf21660e335cb8
Milly <mieelynn.milly@gmail.com>
2022-10-28 22:15:27 +0700

    Modify LoginPage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,6 +2,17 @@ function LoginPage() {
   return (
     <>
       <div className="bg-red-500 font-bold">login</div>
+      <div className="flex">
+        <div className="ml-auto mr-auto flex w-64 flex-col">
+          <input type="text" placeholder="USERNAME" className="rounded-md border-2 p-1" />
+          <input
+            type="password"
+            placeholder="PASSWORD"
+            className="rounded-md border-2 p-1"
+          />
+          <button className=" bg-blue-400">OK</button>
+        </div>
+      </div>
     </>
   )
 }
